104be5bb1631f29c62310f19ee39111155649a46
visionYY <myan_0220@163.com>
2019-03-07 16:34:29 +0800

    0307 my2

--- resources/views/University/Index/index.blade.php
+++ resources/views/University/Index/index.blade.php
@@ -139,10 +139,15 @@
   	</footer>
 		<script>
     var swiper = new Swiper('.swiper-container', {
+			loop: true,
+			autoplay: {
+				delay: 5000,
+				stopOnLastSlide: false,
+				disableOnInteraction: false,
+			},
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
-        loop: true,
       },
     });
     var swiper2 = new Swiper('.swiper-container2', {
