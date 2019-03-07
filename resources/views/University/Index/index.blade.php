82b51102f8ed721ecf6d666bc4200f35f8eefa2a
visionYY <myan_0220@163.com>
2019-03-07 11:50:09 +0800

    0307 my1

--- resources/views/University/Index/index.blade.php
+++ resources/views/University/Index/index.blade.php
@@ -6,88 +6,61 @@
 	<link rel="stylesheet" href="{{asset('University/css/all.css')}}">
 	<link rel="stylesheet" href="{{asset('University/css/index.css')}}">
 
+
 	<style>
-	  /* 清除标签默认边距 */
-	  body,ul,li,ol,img {
-	    margin: 0;
-	    padding: 0;
-	  }
-	  
-	  /* 清除 ul 等标签前面的“小圆点” */
-	  ul,li,ol {
-	    list-style-type: none;
-	  }
+		.swiper-container {
+			width: 100%;
+			height: 100%;
+		}
+		.swiper-container2 {
+			width: 100%;
+			height: 100%;
+		}
+		.swiper-slide {
+			text-align: center;
+			font-size: 18px;
+			background: #fff;
 
-	  /* 图片自适应 */
-	  .box img,.box2 img {
-	    width: 100%;
-	    height: 200px;
-	    border: none;
-	    /* ie8 */
-	    display: block;
-	    -ms-interpolation-mode: bicubic;
-	    /*为了照顾ie图片缩放失真*/
-	  }
-
-	  /* 轮播图最外层盒子 */
-	  .carousel,.carousel2 {
-	    position: relative;
-	    overflow: hidden;
-	  }
-
-	  .carousel ul,.carousel2 ul {
-	    /* 这个高度需要在JS里面动态添加 */
-	  }
-
-	  .carousel ul li,.carousel2 ul li {
-	    position: absolute;
-	    width: 100%;
-	    left: 0;
-	    top: 0;
-	    /* 使用 transform:translaX(300%) 暂时将 li 移动到屏幕外面去*/
-	    -webkit-transform: translateX(300%);
-	    transform: translateX(300%);
-	  }
-
-	  /* 小圆点盒子 */
-	  .carousel .points,.carousel2 .points2 {
-	    /* 未知宽度的盒子，使用 absolute 定位，结合 transform 的方式进行居中 */
-	    position: absolute;
-	    left: 50%;
-	    bottom: 10px;
-	    transform: translateX(-50%);
-	  }
-
-	  /* 小圆点 */
-	  .carousel .points li,.carousel2 .points2 li {
-	    width:.05rem;
-	    height:.025rem;
-	    float:left;
-	    border-radius:.03rem;
-	    background:rgba(125,125,125,1);
-	    margin-left:5px;
-	  }
-
-	  /* 选中小圆点的样式类 */
-	  .carousel .points li.active,.carousel2 .points2 li.active {
-	    width:.08rem;
-	    height:.025rem;
-	    float:left;
-	    border-radius:.03rem;
-	    background:rgba(229,229,229,1);
-	    margin-left:5px;
-	  }
-	</style>
+			/* Center slide text vertically */
+			display: -webkit-box;
+			display: -ms-flexbox;
+			display: -webkit-flex;
+			display: flex;
+			-webkit-box-pack: center;
+			-ms-flex-pack: center;
+			-webkit-justify-content: center;
+			justify-content: center;
+			-webkit-box-align: center;
+			-ms-flex-align: center;
+			-webkit-align-items: center;
+			align-items: center;
+		}
+		.swiper-pagination-bullet {
+			width:8px;
+			height:2.5px;
+			background:rgba(125,125,125,1);
+			border-radius:2px;
+		}
+		.swiper-pagination-bullet-active {
+			background:rgba(229,229,229,1);
+		}
+		.swiper-container-horizontal>.swiper-pagination-bullets, .swiper-pagination-custom, .swiper-pagination-fraction{
+			bottom: 7px;
+		}
+  </style>
 	
 	<div class="wrapper">
 		<div class="banner">
 		  <section class="carousel">
-		    <ul class="box">
-		    	@foreach($adver as $ad)
-		      	<li><a href="{{$ad['href']}}"><img src="{{asset($ad['cover'])}}" alt=""></a></li>
-		      	@endforeach
-		    </ul>
-		    <ol class="points"></ol>
+				<div class="swiper-container">
+					<div class="swiper-wrapper">
+						@foreach($adver as $ad)
+							<div class="swiper-slide"><img src="{{asset($ad['cover'])}}" alt=""></div>
+						@endforeach
+					</div>
+					<!-- Add Pagination -->
+					<div class="swiper-pagination" slot="pagination"></div>
+				</div>
 		  </section>
 		  <!-- <video controls>
 		    <source src="somevideo.webm" type="video/webm">
@@ -126,12 +99,15 @@
 		  <h4 class="bou_tit">精品课</h4>
 		  <div class="banner2">
 		    <section class="carousel2">
-		      <ul class="box2">
-		      	@foreach($course['boutique'] as $bout)
-		        <li><a href="{{url('university/course/show/id/'.$bout->id)}}"><img src="{{url($bout->crosswise_cover)}}" alt=""></a></li>
-		        @endforeach
-		      </ul>
-		      <ol class="points2"></ol>
+					<div class="swiper-container2">
+						<div class="swiper-wrapper">
+						@foreach($course['boutique'] as $bout)
+								<div class="swiper-slide"><img src="{{url($bout->crosswise_cover)}}" alt=""></div>
+							@endforeach
+						</div>
+						<!-- Add Pagination -->
+						<div class="swiper-pagination" slot="pagination"></div>
+					</div>
 		    </section>
 		    <!-- <video controls>
 		      <source src="somevideo.webm" type="video/webm">
@@ -161,325 +137,23 @@
 	    <a href="{{url('university/discussion/index')}}" class="Imgbox"><img src="{{asset('University/images/icon_meiriyiyi@2x.png')}}" />议一议</a>
 	    <a href="{{url('university/my/index')}}" class="Imgbox"><img src="{{asset('University/images/icon_wode@2x.png')}}" />我的</a>
   	</footer>
-	<script>
-	  window.onload = function () {
-	    var carousel2 = document.querySelector('.carousel2');
-	    var carouselUl2 = carousel2.querySelector('.box2');
-	    var carouselLis2 = carouselUl2.querySelectorAll('li');
-	    var points2 = carousel2.querySelector('.points2');
-	    // 屏幕的宽度
-	    var screenWidth2 = document.documentElement.offsetWidth;
-	    var timer2 = null;
+		<script>
+    var swiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        loop: true,
+      },
+    });
+    var swiper2 = new Swiper('.swiper-container2', {
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        loop: true,
+      },
+    });
+  </script>
 
-	    // 设置 ul 的高度
-	    carouselUl2.style.height = carouselLis2[0].offsetHeight + 'px';
-
-	    // 动态生成小圆点
-	    for (var i = 0; i < carouselLis2.length; i++) {
-	      var li2 = document.createElement('li');
-	      if (i == 0) {
-	        li2.classList.add('active');
-	      }
-	      points2.appendChild(li2);
-	    }
-
-	    // 初始三个固定的位置
-	    var left2 = carouselLis2.length - 1;
-	    var center2 = 0;
-	    var right2 = 1;
-
-	    // 归位（多次使用，封装成函数）
-	    setTransform2();
-
-	    // 调用定时器
-	    timer2 = setInterval(showNext2, 2000);
-
-	    // 分别绑定touch事件
-	    var startX2 = 0; // 手指落点
-	    var startTime2 = null; // 开始触摸时间
-	    carouselUl2.addEventListener('touchstart', touchstartHandler2); // 滑动开始绑定的函数 touchstartHandler2
-	    carouselUl2.addEventListener('touchmove', touchmoveHandler2); // 持续滑动绑定的函数 touchmoveHandler2
-	    carouselUl2.addEventListener('touchend', touchendHandeler2); // 滑动结束绑定的函数 touchendHandeler2
-
-	    // 轮播图片切换下一张
-	    function showNext2() {
-	      // 轮转下标
-	      left2 = center2;
-	      center2 = right2;
-	      right2++;
-	      //　极值判断
-	      if (right2 > carouselLis2.length - 1) {
-	        right2 = 0;
-	      }
-	      //添加过渡（多次使用，封装成函数）
-	      setTransition2(1, 1, 0);
-	      // 归位
-	      setTransform2();
-	      // 自动设置小圆点
-	      setPoint2();
-	    }
-
-	    // 轮播图片切换上一张
-	    function showPrev() {
-	      // 轮转下标
-	      right2 = center2;
-	      center2 = left2;
-	      left2--;
-	      //　极值判断
-	      if (left2 < 0) {
-	        left2 = carouselLis2.length - 1;
-	      }
-	      //添加过渡
-	      setTransition2(0, 1, 1);
-	      // 归位
-	      setTransform2();
-	      // 自动设置小圆点
-	      setPoint2();
-	    }
-
-	    // 滑动开始
-	    function touchstartHandler2(e) {
-	      // 清除定时器
-	      clearInterval(timer2);
-	      // 记录滑动开始的时间
-	      startTime2 = Date.now();
-	      // 记录手指最开始的落点
-	      startX2 = e.changedTouches[0].clientX;
-	    }
-	    // 滑动持续中
-	    function touchmoveHandler2(e) {
-	      // 获取差值 自带正负
-	      var dx = e.changedTouches[0].clientX - startX2;
-	      // 干掉过渡
-	      setTransition2(0, 0, 0);
-	      // 归位
-	      setTransform2(dx);
-	    }
-	    //　滑动结束
-	    function touchendHandeler2(e) {
-	      // 在手指松开的时候，要判断当前是否滑动成功
-	      var dx = e.changedTouches[0].clientX - startX2;
-	      // 获取时间差
-	      var dTime = Date.now() - startTime2;
-	      // 滑动成功的依据是滑动的距离（绝对值）超过屏幕的三分之一 或者滑动的时间小于300毫秒同时滑动的距离大于30
-	      if (Math.abs(dx) > screenWidth2 / 3 || (dTime < 300 && Math.abs(dx) > 30)) {
-	        // 滑动成功了
-	        // 判断用户是往哪个方向滑
-	        if (dx > 0) {
-	          // 往右滑 看到上一张
-	          showPrev();
-	        } else {
-	          // 往左滑 看到下一张
-	          showNext2();
-	        }
-	      } else {
-	        // 添加上过渡
-	        setTransition2(1, 1, 1);
-	        // 滑动失败了
-	        setTransform2();
-	      }
-
-	      // 重新启动定时器
-	      clearInterval(timer2);
-	      // 调用定时器
-	      timer2 = setInterval(showNext2, 2000);
-	    }
-	    // 设置过渡
-	    function setTransition2(a, b, c) {
-	      if (a) {
-	        carouselLis2[left2].style.transition = 'transform 1s';
-	      } else {
-	        carouselLis2[left2].style.transition = 'none';
-	      }
-	      if (b) {
-	        carouselLis2[center2].style.transition = 'transform 1s';
-	      } else {
-	        carouselLis2[center2].style.transition = 'none';
-	      }
-	      if (c) {
-	        carouselLis2[right2].style.transition = 'transform 1s';
-	      } else {
-	        carouselLis2[right2].style.transition = 'none';
-	      }
-	    }
-
-	    //　封装归位
-	    function setTransform2(dx) {
-	      dx = dx || 0;
-	      carouselLis2[left2].style.transform = 'translateX(' + (-screenWidth2 + dx) + 'px)';
-	      carouselLis2[center2].style.transform = 'translateX(' + dx + 'px)';
-	      carouselLis2[right2].style.transform = 'translateX(' + (screenWidth2 + dx) + 'px)';
-	    }
-	    // 动态设置小圆点的active类
-	    var pointsLis2 = points2.querySelectorAll('li');
-
-	    function setPoint2() {
-	      for (var i = 0; i < pointsLis2.length; i++) {
-	        pointsLis2[i].classList.remove('active');
-	      }
-	      pointsLis2[center2].classList.add('active');
-	    }
-
-
-
-	    var carousel = document.querySelector('.carousel');
-	    var carouselUl = carousel.querySelector('.box');
-	    var carouselLis = carouselUl.querySelectorAll('li');
-	    var points = carousel.querySelector('.points');
-	    // 屏幕的宽度
-	    var screenWidth = document.documentElement.offsetWidth;
-	    var timer = null;
-
-	    // 设置 ul 的高度
-	    carouselUl.style.height = carouselLis[0].offsetHeight + 'px';
-
-	    // 动态生成小圆点
-	    for (var i = 0; i < carouselLis.length; i++) {
-	      var li = document.createElement('li');
-	      if (i == 0) {
-	        li.classList.add('active');
-	      }
-	      points.appendChild(li);
-	    }
-
-	    // 初始三个固定的位置
-	    var left = carouselLis.length - 1;
-	    var center = 0;
-	    var right = 1;
-
-	    // 归位（多次使用，封装成函数）
-	    setTransform();
-
-	    // 调用定时器
-	    timer = setInterval(showNext, 2000);
-
-	    // 分别绑定touch事件
-	    var startX = 0; // 手指落点
-	    var startTime = null; // 开始触摸时间
-	    carouselUl.addEventListener('touchstart', touchstartHandler); // 滑动开始绑定的函数 touchstartHandler
-	    carouselUl.addEventListener('touchmove', touchmoveHandler); // 持续滑动绑定的函数 touchmoveHandler
-	    carouselUl.addEventListener('touchend', touchendHandeler); // 滑动结束绑定的函数 touchendHandeler
-
-	    // 轮播图片切换下一张
-	    function showNext() {
-	      // 轮转下标
-	      left = center;
-	      center = right;
-	      right++;
-	      //　极值判断
-	      if (right > carouselLis.length - 1) {
-	        right = 0;
-	      }
-	      //添加过渡（多次使用，封装成函数）
-	      setTransition(1, 1, 0);
-	      // 归位
-	      setTransform();
-	      // 自动设置小圆点
-	      setPoint();
-	    }
-
-	    // 轮播图片切换上一张
-	    function showPrev() {
-	      // 轮转下标
-	      right = center;
-	      center = left;
-	      left--;
-	      //　极值判断
-	      if (left < 0) {
-	        left = carouselLis.length - 1;
-	      }
-	      //添加过渡
-	      setTransition(0, 1, 1);
-	      // 归位
-	      setTransform();
-	      // 自动设置小圆点
-	      setPoint();
-	    }
-
-	    // 滑动开始
-	    function touchstartHandler(e) {
-	      // 清除定时器
-	      clearInterval(timer);
-	      // 记录滑动开始的时间
-	      startTime = Date.now();
-	      // 记录手指最开始的落点
-	      startX = e.changedTouches[0].clientX;
-	    }
-	    // 滑动持续中
-	    function touchmoveHandler(e) {
-	      // 获取差值 自带正负
-	      var dx = e.changedTouches[0].clientX - startX;
-	      // 干掉过渡
-	      setTransition(0, 0, 0);
-	      // 归位
-	      setTransform(dx);
-	    }
-	    //　滑动结束
-	    function touchendHandeler(e) {
-	      // 在手指松开的时候，要判断当前是否滑动成功
-	      var dx = e.changedTouches[0].clientX - startX;
-	      // 获取时间差
-	      var dTime = Date.now() - startTime;
-	      // 滑动成功的依据是滑动的距离（绝对值）超过屏幕的三分之一 或者滑动的时间小于300毫秒同时滑动的距离大于30
-	      if (Math.abs(dx) > screenWidth / 3 || (dTime < 300 && Math.abs(dx) > 30)) {
-	        // 滑动成功了
-	        // 判断用户是往哪个方向滑
-	        if (dx > 0) {
-	          // 往右滑 看到上一张
-	          showPrev();
-	        } else {
-	          // 往左滑 看到下一张
-	          showNext();
-	        }
-	      } else {
-	        // 添加上过渡
-	        setTransition(1, 1, 1);
-	        // 滑动失败了
-	        setTransform();
-	      }
-
-	      // 重新启动定时器
-	      clearInterval(timer);
-	      // 调用定时器
-	      timer = setInterval(showNext, 2000);
-	    }
-	    // 设置过渡
-	    function setTransition(a, b, c) {
-	      if (a) {
-	        carouselLis[left].style.transition = 'transform 1s';
-	      } else {
-	        carouselLis[left].style.transition = 'none';
-	      }
-	      if (b) {
-	        carouselLis[center].style.transition = 'transform 1s';
-	      } else {
-	        carouselLis[center].style.transition = 'none';
-	      }
-	      if (c) {
-	        carouselLis[right].style.transition = 'transform 1s';
-	      } else {
-	        carouselLis[right].style.transition = 'none';
-	      }
-	    }
-
-	    //　封装归位
-	    function setTransform(dx) {
-	      dx = dx || 0;
-	      carouselLis[left].style.transform = 'translateX(' + (-screenWidth + dx) + 'px)';
-	      carouselLis[center].style.transform = 'translateX(' + dx + 'px)';
-	      carouselLis[right].style.transform = 'translateX(' + (screenWidth + dx) + 'px)';
-	    }
-	    // 动态设置小圆点的active类
-	    var pointsLis = points.querySelectorAll('li');
-
-	    function setPoint() {
-	      for (var i = 0; i < pointsLis.length; i++) {
-	        pointsLis[i].classList.remove('active');
-	      }
-	      pointsLis[center].classList.add('active');
-	    }
-	  }
-	</script>
   
     
 @stop
